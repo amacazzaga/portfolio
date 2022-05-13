b1aa05a3ac3c58ba275977cc9cb6192d98462c28
unknown <ag.macazz@gmail.com>
2022-05-13 17:16:07 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+  return <div className="container">hello</div>;
 }
-
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+  crossorigin="anonymous"
+></script>;
 export default App;
