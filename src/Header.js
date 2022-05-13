77f72b950a689a9460c4b2082a5954b9786677c8
unknown <ag.macazz@gmail.com>
2022-05-13 17:25:37 -0300

    header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header' >
+    <p>hello,world, this is my portfolio!</p>
+    <h1>Agustin Macazzaga, Front End Software Developer</h1>
+   </div>
+  )
+}
+
+export default Header
