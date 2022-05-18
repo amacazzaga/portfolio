a6fa8e814f49e1f4d4669b660ebc1e0b7ee01716
unknown <ag.macazz@gmail.com>
2022-05-18 11:24:10 -0300

    items footer align on center

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-         <ul>
+    <div className="footer">
+      <div className="contact-div">
+        <h4>CONTACT</h4>
+      </div>
+      <div className="ul-container">
+      <ul>
         <li className="navbar">
-          <a href="https://www.linkedin.com/in/agustin-macazzaga-675b3a1b3/">Linkedin</a>
+          <a href="https://www.linkedin.com/in/agustin-macazzaga-675b3a1b3/">
+            Linkedin
+          </a>
         </li>
         <li className="navbar">
           <a href="https://github.com/amacazzaga">Github</a>
@@ -15,7 +21,8 @@ const Footer = () => {
         </li>
       </ul>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
