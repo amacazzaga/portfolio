16d9142f59ce16ee1a93ccedbb18ad2409a0af03
unknown <ag.macazz@gmail.com>
2022-05-19 11:48:04 -0300

    description button with state

--- src/Proyect.js
+++ src/Proyect.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Proyect = () => {
-  return (
-    <div className='proyect'>Proyect</div>
-  )
-}
+  const [state, setState] = useState("collapse");
 
-export default Proyect
+  return (
+    <div className="proyect">
+      Proyect
+      <p>
+        <button
+          onClick={() => {
+            if (state === "collapse") {
+              setState("collapse.show");
+            } else if (state === "collapse.show") {
+              setState("collapse");
+            }
+          }}
+          class="btn btn-primary"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseExample"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+         Description
+        </button>
+      </p>
+      <div className={state} id="collapseExample">
+        <div class="card card-body">
+         This game is a classic all time called simonSays
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Proyect;
