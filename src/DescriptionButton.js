866b9e2e02a08d102a50d46fbe2f3b9ed8967a24
unknown <ag.macazz@gmail.com>
2022-05-21 20:14:56 -0300

    padding on button description

--- src/DescriptionButton.js
+++ src/DescriptionButton.js
@@ -1,36 +1,38 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 const DescriptionButton = () => {
-    const [state, setState] = useState("collapse");
+  const [state, setState] = useState("collapse");
   return (
-    <div> <div className="button-description-container">
-    <button
-      className="button-description"
-      onClick={() => {
-        if (state === "collapse") {
-          setState("collapse.show");
-        } else if (state === "collapse.show") {
-          setState("collapse");
-        }
-      }}
-      class="btn btn-primary"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#collapseExample"
-      aria-expanded="false"
-      aria-controls="collapseExample"
-    >
-      Description
-    </button>
-  </div>
+    <div>
+      {" "}
+      <div className="button-description-container">
+        <button
+          className="button-description"
+          onClick={() => {
+            if (state === "collapse") {
+              setState("collapse.show");
+            } else if (state === "collapse.show") {
+              setState("collapse");
+            }
+          }}
+          class="btn btn-primary"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseExample"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          Description
+        </button>
+      </div>
+      <div className={state} id="collapseExample">
+        <div class="card card-body">
+          This game is a classic all time called simonSays
+        </div>
+      </div>
+    </div>
+  );
+};
 
-<div className={state} id="collapseExample">
-  <div class="card card-body">
-    This game is a classic all time called simonSays
-  </div>
-</div></div>
-  )
-}
-
-export default DescriptionButton
+export default DescriptionButton;
