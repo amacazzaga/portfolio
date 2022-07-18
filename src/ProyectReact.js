d06af59bd4cf03cbab99c3b37ee79fe6846d9729
unknown <ag.macazz@gmail.com>
2022-07-18 12:36:14 -0300

    proyects on cards

--- src/ProyectReact.js
+++ src/ProyectReact.js
@@ -12,28 +12,46 @@ const ProyectReact = ({
   description
 }) => {
   return (
-    <div className="proyect">
-      <a href={href}>
-        <img className="img-fluid-proyect" src={img}></img>
-      </a>
-      <div className="container-img">
-        <div>
-          <img className="img-fluid-proyectReact" src={JavascriptLogo} alt="" />
+    <div className="card proyect" >
+      <div class="card-body">
+        <a href={href}>
+          <img className="card-img-top img-fluid-proyect" src={img}></img>
+        </a>
+        <div className="container-img">
+          <div>
+            <img
+              className="img-fluid-proyectVJavascript"
+              src={JavascriptLogo}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="img-fluid-proyectVJavascript"
+              src={HtmlLogo}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="img-fluid-proyectVJavascript"
+              src={CssLogo}
+              alt=""
+            />
+          </div>
         </div>
+        <DescriptionButton description={description} />
         <div>
-          <img className="img-fluid-proyectReact" src={ReactLogo} alt="" />
-        </div>
-        <div>
-          <img className="img-fluid-proyectReact" src={HtmlLogo} alt="" />
-        </div>
-        <div>
-          <img className="img-fluid-proyectReact" src={CssLogo} alt="" />
-        </div>
-        <div>
-          <img className="img-fluid-proyectReact" src={BootstrapLogo} alt="" />
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
         </div>
       </div>
-      <DescriptionButton description={description}/>
     </div>
   );
 };
--- src/ProyectVJavascript.js
+++ src/ProyectVJavascript.js
@@ -10,26 +10,46 @@ const ProyectVJavascript = ({
   description,
 }) => {
   return (
-    <div className="proyect">
-      <a href={href}>
-        <img className="img-fluid-proyect" src={img}></img>
-      </a>
-      <div className="container-img">
-        <div>
-          <img
-            className="img-fluid-proyectVJavascript"
-            src={JavascriptLogo}
-            alt=""
-          />
+    <div className="card proyect" >
+      <div class="card-body">
+        <a href={href}>
+          <img className="card-img-top img-fluid-proyect" src={img}></img>
+        </a>
+        <div className="container-img">
+          <div>
+            <img
+              className="img-fluid-proyectVJavascript"
+              src={JavascriptLogo}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="img-fluid-proyectVJavascript"
+              src={HtmlLogo}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="img-fluid-proyectVJavascript"
+              src={CssLogo}
+              alt=""
+            />
+          </div>
         </div>
+        <DescriptionButton description={description} />
         <div>
-          <img className="img-fluid-proyectVJavascript" src={HtmlLogo} alt="" />
-        </div>
-        <div>
-          <img className="img-fluid-proyectVJavascript" src={CssLogo} alt="" />
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
         </div>
       </div>
-      <DescriptionButton description={description} />
     </div>
   );
 };
